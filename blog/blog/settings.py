6ad93aa033e939763bf76e9c864bccc8b3f3da80
tuipik <tuipiku@gmail.com>
2019-03-30 14:14:53 +0200

    almost done

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -160,9 +160,9 @@ SUIT_CONFIG = {
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/1')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/staticfiles"),
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
